a5d433937e040a3010b5828db54f964a13aab9b1
Eitasam Butt <117992543+EitasamButt101@users.noreply.github.com>
2022-12-30 19:03:55 +0500

    first commit

--- README.md
+++ README.md
@@ -9,6 +9,5 @@ The link of my server is http://localhost:3500 u can test from here
 when open this link i got 2 request 1 from that route and other for the static file that we specified that is available on public folder
 so we have reqlog in our logs folder
 we have date time and specific id of the request
-# tec
-# tr
+we connected to Our mangoDB DataBase
 # techNotes-Project
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const path = require('path')
 const { logger } = require('./middleware/logger')
@@ -6,7 +7,17 @@ const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions = require('./logs/corsOptions')
+const connectDB = require ('./logs/dbConn')
+const mongoose = require('mongoose')
+const {logEvents} = require('./middleware/logger')
+
 const PORT = process.env.PORT || 3500
+
+mongoose.set('strictQuery', true);
+
+
+console.log(process.env.NODE_ENV)
+connectDB()
 
 app.use(logger)
 
@@ -32,5 +43,13 @@ app.all('*', (req, res) => {
 })
 
 app.use(errorHandler)
+mongoose.connection.once('open', ()=> {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+mongoose.connection.on('error', err =>{
+console.log(err)
+logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
